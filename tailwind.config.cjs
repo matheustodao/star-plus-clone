962b386c2d2de0cb1413e4999f683f3ed44fecd4
matheustodao <todaomatheus@hotmail.com>
2023-02-08 00:50:04 -0300

    chore: setup application -  Initialize React App from Vite - Setup Husky and Lint-Staged - Setup Eslint - Setup tailwindCSS - Setup Prettier - Setup EditorConfig

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/presentation/components/*.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
