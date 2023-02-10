407a11ff5c994984b666d4b23e3c785cfe72fc09
matheustodao <todaomatheus@hotmail.com>
2023-02-10 18:19:06 -0300

    refactor: add second page step sign up form

---
+++ src/presentation/pages/sign-up/subpages/subscriber-agreement-review/index.tsx
@@ -0,0 +1,179 @@
+import { SignUpContainer as Container } from '@components/Sign-Up/Container';
+import { HeaderFormSignUp } from '@components/Sign-Up/form/Header';
+
+export function SubscriberAgreementReview() {
+  return (
+    <Container className="mb-10 min-[480px]:h-screen">
+      <div className="min-[480px]:max-w-[506px] min-[480px]:mx-auto">
+        <HeaderFormSignUp currentStep={2} title="Subscriber Agreement" />
+
+        <div className="min-[480px]:border-[1px] min-[480px]:border-[#1a1d29] relative">
+          <p className="text-xs min-[480px]:overflow-y-scroll min-[480px]:bg-bg-default min-[480px]:max-h-[260px] min-[480px]:p-5">
+            Atualizado: 27 de julho de 2021 <br />
+            <br />
+            The Walt Disney Company (Brasil) Ltda. (“Disney”, “nós”, “nosso/a”,
+            “a gente”), com sede em World Trade Center, Av. Das Nações Unidas,
+            12.551, 12.555, 12.559, Piso 10, São Paulo/SP – CEP 04578-903,
+            Brasil e CNPJ/M 73.042.962/0004-20 lhe dá as boas-vindas ao Serviço
+            Disney+ e/ou STAR+. <br />
+            <br />
+            (i) O “Serviço Disney+” é um serviço de streaming de vídeo
+            personalizado por assinatura, que inclui o site Disney+ e o
+            aplicativo, bem como os conteúdos e serviços associados.
+            <br />
+            <br />
+            (ii) O “Serviço STAR+” é um serviço de streaming de vídeo
+            personalizado por assinatura, que inclui o site STAR+ e o
+            aplicativo, bem como os conteúdos e serviços associados.
+            <br />
+            <br />
+            ESTE CONTRATO REGULARÁ O USO DO SERVIÇO DISNEY+ E/OU SERVIÇO STAR+.
+            POR FAVOR, LEIA ESTE CONTRATO DE ASSINATURA (“CONTRATO DE
+            ASSINATURA” OU “CONTRATO”) COM CUIDADO ANTES DE USAR O SERVIÇO
+            DISNEY+ E/OU SERVIÇO STAR+ (CONJUNTAMENTE “SERVIÇOS”).
+            <br />
+            <br />
+            ESTE CONTRATO DE ASSINATURA INCLUI TERMOS E CONDIÇÕES QUE SÃO COMUNS
+            AOS DOIS SERVIÇOS, BEM COMO TERMOS E CONDIÇÕES ADICIONAIS QUE PODEM
+            SE APLICAR A SOMENTE UM DOS SERVIÇOS. TANTO OS TERMOS COMUNS, QUANTO
+            OS TERMOS ADICIONAIS SERÃO CLARAMENTE IDENTIFICADOS.
+            <br />
+            <br />
+            PARA EVITAR DÚVIDAS, QUANDO O CONTRATO DE ASSINATURA SE REFERIR AOS
+            SERVIÇOS POR SEU NOME (COMO SERVIÇO DISNEY+ OU SERVIÇO STAR+), TAL
+            REFERÊNCIA NÃO DEVERÁ SER INTERPRETADA DE MANEIRA A INCLUIR O OUTRO
+            SERVIÇO.
+            <br />
+            <br />A ACEITAÇÃO DO CONTRATO DE ASSINATURA NÃO ENVOLVE E NÃO SERÁ
+            INTERPRETADA COM ENVOLVENDO A PRESTAÇÃO DE UM SERVIÇO QUE VOCÊ NÃO
+            OPTOU POR ASSINAR. A ACEITAÇÃO DO CONTRATO DE ASSINATURA ESTÁ
+            LIMITADA AO(S) SERVIÇO(S) AOS QUAIS VOCÊ ASSINOU.
+            <br />
+            <br />
+            AO CLICAR EM “CONCORDAR E CONTINUAR” DURANTE O PROCESSO DE REGISTRO
+            AO SERVIÇO DISNEY+ E/OU SERVIÇO STAR+ OU QUALQUER OUTRO MECANISMO
+            RELACIONADO A ESSE TIPO DE SERVIÇO, VOCÊ CONCORDA PLENAMENTE E SEM
+            RESERVAS COM ESTE CONTRATO DE ASSINATURA E RECONHECE QUE VOCÊ LEU E
+            ENTENDEU A NOSSA POLÍTICA DE PRIVACIDADE, CONFIRMANDO A CONCORDÂNCIA
+            AO DESFRUTAR DE QUALQUER ELEMENTO DO SERVIÇO. CASO VOCÊ NÃO CONCORDE
+            COM ESTE CONTRATO DE ASSINATURA, VOCÊ NÃO PODERÁ FAZER USO DO
+            SERVIÇO DISNEY+ E/OU SERVIÇO STAR+. <br />
+            <br />
+            Este Contrato pode ser aditado por nós no futuro a nosso exclusivo
+            critério. Qualquer aditamento que afete seus direitos ou obrigações
+            passará a surtir efeitos no prazo de 30 (trinta) dias após o envio
+            de notificação nossa para o e-mail indicado por você no ato da sua
+            inscrição. Caso o e-mail indicado por você não esteja habilitado
+            para receber nossas notificações, nós enviaremos um aviso pelo
+            Serviço. Caso você não concorde com as alterações feitas da forma
+            acima ao Contrato de Assinatura, você poderá cancelar sua assinatura
+            do Serviço Disney+ e/ou Serviço STAR+, conforme os termos e
+            condições especificadas na cláusula 2 abaixo. Ademais, certos
+            elementos do Serviço Disney+ e/ou Serviço STAR+ poderão ser
+            alterados a qualquer momento e sem notificação prévia, por exemplo
+            relacionados a evoluções técnicas, contanto que não haja incremento
+            ao preço e não haja alteração de qualidade ou modificação
+            substancial às funcionalidades do Serviço Disney+ e/ou Serviço
+            STAR+.
+            <br />
+            <br />
+            Para os fins do presente Contrato de Assinatura, o Serviço Disney+ e
+            o Serviço STAR+ poderão ser chamados, coletivamente, de “Serviço” ou
+            “Serviços”.
+            <br />
+            <br />
+            Nossos representantes de atendimento ao cliente não estão
+            autorizados a modificar qualquer aspecto dos termos e condições do
+            Contrato de Assinatura e dos documentos a ele relacionados, seja
+            verbalmente ou por escrito.
+            <br />
+            <br />
+            TERMOS E CONDIÇÕES COMUNS AO SERVIÇO
+            <br />
+            <br />
+            Lista de Termos e Condições Comuns aos Serviços:
+            <br />
+            1. Condições de Admissibilidade e Registro do Usuário
+            <br />
+            2. Termos para Assinatura
+            <br />
+            3. Concessões e Restrições da Licença de Direitos Autorais e
+            Copyrights
+            <br />
+            4. Termos de Uso <br />
+            5. Uso e Divulgação de Suas Informações <br />
+            6. Suspensão e Rescisão
+            <br />
+            7. Outras Disposições
+            <br />
+            <br />
+            1. CONDIÇÕES DE ADMISSIBILIDADE E REGISTRO DO USUÁRIO
+            <br />
+            <br />
+            a. Admissibilidade e Limitações por Idade. Apenas residentes nos
+            países em que oferecemos o Serviço (coletivamente, o “Território”)
+            são elegíveis para o registro como usuários de uma conta do Serviço.
+            Este Contrato de Assinatura se aplica aos residentes do Brasil. Você
+            precisa ser maior de idade para adquirir o Serviço. Menores de idade
+            e civilmente incapazes para celebrar contratos, empresas,
+            associações e outras entidades não podem se registrar para a
+            obtenção de uma conta do Serviço ou usar o Serviço e não são
+            permitidos a fornecer informações pessoais à Disney. Como assinante,
+            você será responsável pelos atos de todos sob seus cuidados enquanto
+            faz/fazem uso do Serviço. Ao acessar e utilizar o Serviço, você
+            concorda e confirma que é maior de idade e possui capacidade civil
+            para se registrar e assinar o Serviço. O Serviço é oferecido apenas
+            a pessoas físicas para uso pessoal e não-comercial.
+            <br />
+            <br />
+            b. Registro e Segurança. Somente pessoas físicas que se registrem
+            para uma conta do Serviço, forneçam informações específicas (por
+            ex.: e-mail válido) e que concordem com o presente Contrato de
+            Assinatura estão aptos a usar o Serviço. A Disney adotará medidas
+            técnicas e organizacionais para garantir a segurança de seus
+            sistemas e processos. <br /> <br />
+            Você é exclusivamente responsável por manter a confidencialidade e
+            segurança de seu nome de usuário e senha, bem como por todas as
+            atividades que venham a ocorrer em sua conta do Serviço No entanto,
+            caso você conceda acesso à sua conta do Serviço para terceiros, este
+            Contrato se aplicará ao acesso deles, uso e divulgação de informação
+            por eles. Você concorda em notificar a Disney imediatamente sobre
+            qualquer acesso não autorizado à sua conta do Serviço. A Disney não
+            se responsabilizará por quaisquer perdas e danos originadas do uso
+            não autorizado da sua conta do Serviço.
+            <br />
+            <br />
+            c. Avisos. Quaisquer avisos feitos pela Disney+ a você poderão ser
+            feitos das seguintes formas: (i) via e-mail ao último endereço de
+            e-mail indicado por você e/ou (ii) por meio de uma postagem no
+            Serviço. Você concorda em oferecer e manter informações cadastrais
+            atualizadas, completas e precisas, incluindo informações de contato
+            para o envio de avisos e outras
+          </p>
+
+          <div
+            className="sticky bottom-0 left-0 w-[100vw] flex flex-col items-center py-6 bg-[#1a1d29]
+            shadow-[0px_-28px_35px_-3px_rgba(26,29,42,0.9)] -ml-5 border-t-[1px]
+          border-t-bg-gray-50/10 mt-6
+            min-[480px]:shadow-none box-content min-[480px]:static min-[480px]:box-border min-[480px]:w-full min-[480px]:ml-0 min-[480px]:pt-0
+            min-[480px]:mt-0"
+          >
+            <small className="mb-4 text-xs text-center leading-[1.5] px-5">
+              By clicking "Agree & Continue", you confirm that you have read in
+              full and agree to our Subscriber Agreement.
+            </small>
+
+            <div className="w-full flex items-center justify-center">
+              <button
+                type="button"
+                className="btn--primary btn--inline-flex mx-auto min-[480px]:w-[65%]"
+              >
+                AGREE & CONTINUE
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+}
--- src/presentation/router/routes/sign-up.tsx
+++ src/presentation/router/routes/sign-up.tsx
@@ -1,6 +1,7 @@
 import { LayoutRoot } from '@presentation/components/shared/Layout';
 import { SignUpPage } from '@presentation/pages/sign-up';
 import { SignUpPasswordPage } from '@presentation/pages/sign-up/subpages/PasswordPage';
+import { SubscriberAgreementReview } from '@presentation/pages/sign-up/subpages/subscriber-agreement-review';
 import { RouteObject } from 'react-router-dom';
 
 export const RoutingSignUpIndex: RouteObject = {
@@ -21,6 +22,11 @@ export const RoutingSignUp: RouteObject = {
     {
       path: 'password',
       element: <SignUpPasswordPage />,
+    },
+
+    {
+      path: 'subscriber-agreement-review',
+      element: <SubscriberAgreementReview />,
     },
   ],
 };
