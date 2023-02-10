2811a9e5b7438f617601e7b2ea371e326351a2e7
matheustodao <todaomatheus@hotmail.com>
2023-02-10 06:04:49 -0300

    refactor: margin left delete

--- src/presentation/components/shared/Header/Public/Default/index.tsx
+++ src/presentation/components/shared/Header/Public/Default/index.tsx
@@ -4,7 +4,7 @@ export function HeaderPublic() {
   return (
     <header className="flex items-center justify-end pr-3 mt-10">
       <div className="w-full flex items-center justify-center">
-        <div className="max-w-[148px] max-h-[89px] ml-20">
+        <div className="max-w-[148px] max-h-[89px]">
           <img src={logo} alt="Star Plus Logo" width="100%" height="100%" />
         </div>
       </div>
