7b51b3e92e18fc2f0f68b9966d09439fd1758540
matheustodao <todaomatheus@hotmail.com>
2023-02-10 03:45:11 -0300

    refactor: change variant color schema

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 
+const roseSchema = {
+  600: 'rgb(225, 5, 74)',
+}
+
+const redSchema = {
+  600: 'rgb(225, 85, 76)',
+}
+
+const graySchema = {
+  50: '#F9F9F9',
+  300: '#CACACA'
+}
+
 module.exports = {
   content: [
-    "./src/presentation/components/**/*.tsx"
+    "./src/presentation/components/**/*.tsx",
+    "./src/presentation/pages/**/*.tsx"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: {
+          default: '#0E0B13',
+          gray: graySchema,
+          red: redSchema,
+          rose: roseSchema,
+        },
+
+        gray: graySchema,
+        red: redSchema,
+        rose: roseSchema,
+      }
+    },
   },
   plugins: [],
 }
